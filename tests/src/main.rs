b1e151b9a22f28722a0ef32b307dd7d7d2697746
Dravia Vemal <contact@draviavemal.com>
2025-08-16 07:37:45 +0000

    Reorganizing crate for test setup and link to openxml-office

---
+++ tests/src/main.rs
@@ -0,0 +1,8 @@
+// mod document;
+// mod presentation;
+// mod spreadsheet;
+mod xml;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ tests/src/xml.rs
@@ -0,0 +1,41 @@
+#[cfg(test)]
+mod xml_test {
+    use draviavemal_xml_rs::{XmlDeSerializer, XmlSerializer};
+
+    #[test]
+    fn test_xml_deserialization() {
+        let xml_input = r#"
+        <test:catalog>
+            <!-- This is Test Content 1 -->
+            <!-- This is Test Content 2 -->
+            <book id="bk101">
+                <author>John Doe</author>
+                <title>XML Basics</title>
+                <genre>Programming</genre>
+                <price>29.95</price>
+                <empty/>
+                <newempty attr1="value" attr2="value2"/>
+                <publish_date>
+                    <date>01</date>
+                    <month>01</month>
+                    <year>2024</year>
+                </publish_date>
+                <description>An introduction to XML.</description>
+            </book>
+            <book id="bk102">
+                <author>Jane Smith</author>
+                <title>Advanced XML</title>
+                <genre>Programming</genre>
+                <price>39.95</price>
+                <publish_date>2024-06-02</publish_date>
+                <description>Deep dive into XML technologies.</description>
+            </book>
+        </test:catalog>
+    "#;
+
+        let mut document = XmlDeSerializer::vec_to_xml_doc_tree(xml_input.as_bytes().to_vec())
+            .expect("Failed to deserialize XML");
+        let content = XmlSerializer::xml_tree_to_vec(&mut document).expect("Failed to serialize XML");
+        assert!(true);
+    }
+}
